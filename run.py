366a2a0e54c1397ae721e1aea394e5d98c8daa39
Mohammad Tahmasebi <mohammad.tahmasebi@students.codeinstitute.net>
2024-12-06 05:42:57 +0000

    Add falsh messages using Flask

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import os
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+if os.path.exists('env.py'):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY')
 
 @app.route('/')
 def index():
@@ -31,7 +34,10 @@ def about_member(member_name):
 @app.route('/contact', methods=['GET','POST'])
 def contact():
     if request.method == 'POST':
-        print(request.form.get('name'))
+    #    print(request.form.get('name'))  # if name key does not exist then we get None 
+    #    print(request.form['email'])     # if email key does not exist then we get Exception
+        flash('Thanks {}, we have received your message!'.format(
+            request.form.get('name')))
     return render_template('contact.html', page_title='Contact us')
 
 
